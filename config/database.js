12a3ae2db34bcd65a390b515512eb09403185aa3
Kazuo <r.kazuo.f@gmail.com>
2018-10-19 11:02:10 -0300

    ajuste para reconnect no banco sempre que disconectar

--- config/database.js
+++ config/database.js
@@ -2,27 +2,31 @@ module.exports = function(uri) {
 
 	var mongoose = require('mongoose');
 
-	mongoose.connect(uri);
-
-	mongoose.connection.on('connected', function() {
-		console.log('Conected to MongoDB')
+	mongoose.connection.on('connecting', () => {
+		console.log('Connecting to MongoDB...');
 	});
 
-	mongoose.connection.on('error', function(error) {
+	mongoose.connection.on('connected', () => {
+		console.log('Conected to MongoDB');
+	});
+
+	mongoose.connection.on('error', error => {
 		console.log('Connection Error: ' + error);
 	});	
 
-	mongoose.connection.on('disconnected', function() {
-		console.log('Disconnected from MongoDB')
+	mongoose.connection.on('disconnected', () => {
+		console.log('Disconnected from MongoDB. Trying to reconnect...');
+		setTimeout(() => mongoose.connect(uri, {server:{auto_reconnect:true}}), 3000);
 	});
 
-	process.on('SIGINT', function() {
+	process.on('SIGINT', () => {
 		mongoose.connection.close(function() {
 			console.log('Application terminated, connection closed')
 			process.exit(0);
 		});
-		
 	})
+
+	mongoose.connect(uri, {server:{auto_reconnect:true}});
 };
 
 
